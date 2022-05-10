38b905fef08b270deb714e484e7fd5a9310540c4
Rajarshi Sarkar <rajsarkar799@gmail.com>
2022-05-10 20:24:38 +0530

    01_color-flipper: exercise to do completed

--- 01_color-flipper/app.js
+++ 01_color-flipper/app.js
@@ -3,11 +3,22 @@ const colors = ["green", "red", "lightblue", "lightsalmon", "lightsteelblue"];
 const btn = document.getElementById("btn");
 // TODO 1: use "getElementsByClassName" instead of "querySelector" and make changes accordingly
 
-const color = document.querySelector(".color");
-
+// const color = document.querySelector(".color");
+const color = document.getElementsByClassName("color")[0];
 // TODO 2: use arrow function in function definitions
 
-btn.addEventListener('click', function () {
+// btn.addEventListener('click', function () {
+//   const randomNumber = getRandomNumber();
+//   console.log(randomNumber);
+//   document.body.style.backgroundColor = colors[randomNumber];
+//   color.textContent = colors[randomNumber];
+// });
+
+// function getRandomNumber() {
+//   return Math.floor(Math.random() * colors.length);
+// }
+
+btn.addEventListener('click', () => {
   const randomNumber = getRandomNumber();
   console.log(randomNumber);
   document.body.style.backgroundColor = colors[randomNumber];
