0e3a4c58f7fa2f83c4d7136b58cede0e866e4115
Rajarshi Sarkar <rajsarkar799@gmail.com>
2022-05-20 20:43:52 +0530

    02_counter: add style.css and completed exercise to do

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -6,21 +6,29 @@ const value = document.getElementById("value");
 const btns = document.querySelectorAll(".btn");
 
 // TODO 01: use arrow function
-btns.forEach(function (btn) {
-  btn.addEventListener('click', function (e) {
-    const styles = e.currentTarget.classList;
+// btns.forEach(function (btn) {
+//   btn.addEventListener('click', function (e) {
+//     const styles = e.currentTarget.classList;
+
+btns.forEach( (btn) => {
+   btn.addEventListener('click', (e) => {
+     const styles = e.currentTarget.classList;
 
      // TODO 02: use ternary operator
-    if (styles.contains("increase")) count++;
-    else if (styles.contains("decrease")) count--;
-    else count = 0;
-    value.textContent = count;
-
+    // if (styles.contains("increase")) count++;
+    // else if (styles.contains("decrease")) count--;
+    // else count = 0;
+    // value.textContent = count;
+     
+     (styles.contains("increase")) ? count++ : (styles.contains("decrease")) ? count-- : count = 0;
+      value.textContent = count;
     // TODO 03: use ternary operator
     //  change the color of the value wrt buttons
 
-    if (count > 0) value.style.color = "green";
-    else if (count < 0) value.style.color = "red";
-    else value.style.color = "black";
+    // if (count > 0) value.style.color = "green";
+    // else if (count < 0) value.style.color = "red";
+    // else value.style.color = "black";
+     
+     (count > 0) ? (value.style.color = "green") : (count < 0) ? (value.style.color = "red") : value.style.color = "black"; 
   });
 })
